9e8fd248718899fe72ba47226bd0dd6a85e3aac1
Jean Parpaillon <jean.parpaillon@free.fr>
2021-12-16 15:54:48 +0100

    Initial cleanup

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Goodies
 
-**TODO: Add description**
-
+Small volume manager
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
--- lib/goodies.ex
+++ lib/goodies.ex
@@ -2,17 +2,4 @@ defmodule Goodies do
   @moduledoc """
   Documentation for `Goodies`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Goodies.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
